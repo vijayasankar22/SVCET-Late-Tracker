d5157eb0d145524a59c884648493cf5a633e2d23
Vijayasankar <vijayasankar2225@gmail.com>
2025-09-20 16:29:48 +0000

    change the title icon in tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const spaceGrotesk = Space_Grotesk({ subsets: ['latin'], variable: '--font-space
 export const metadata: Metadata = {
   title: 'SVCET - Late Tracker',
   description: 'Late Student Tracker',
+  icons: {
+    icon: `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📋</text></svg>`
+  }
 };
 
 export default function RootLayout({
