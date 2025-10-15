a4fa8db15f7a5b3c3a42a6e1966de85c3d522222
Vijayasankar <vijayasankar2225@gmail.com>
2025-10-15 05:46:04 +0000

    the same error is repeating again and again do something and solve it ,

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    pwa: {
-      dest: 'public',
-      register: true,
-      skipWaiting: true,
-    },
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
