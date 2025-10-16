1051824c370777bc16ffa0d396841631fed1a191
Vijayasankar <vijayasankar2225@gmail.com>
2025-10-16 14:31:40 +0000

    remove the happy diwali text feature

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,15 +89,10 @@ export default {
             height: '0',
           },
         },
-         marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'marquee': 'marquee 25s linear infinite',
       },
     },
   },
