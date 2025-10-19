1a9e6cf898b98c99ea024e5f30c59fd03aef7b9b
Vijayasankar <vijayasankar2225@gmail.com>
2025-10-19 13:29:54 +0000

    back to previous

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={`${inter.variable} ${spaceGrotesk.variable} font-body antialiased bg-primary`} suppressHydrationWarning>
+      <body className={`${inter.variable} ${spaceGrotesk.variable} font-body antialiased`} suppressHydrationWarning>
           <AuthProvider>
             {children}
           </AuthProvider>
